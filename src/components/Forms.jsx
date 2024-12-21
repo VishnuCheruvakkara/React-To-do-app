d1561f0e314ffb2218f126b1465f70c14de03802
vishnu cheruvakkara <cheruvakkarav@gmail.com>
2024-12-21 18:05:58 +0530

    Multi input form creation done

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -3,12 +3,13 @@ import React, { Component } from "react";
 
 class Forms extends Component {
     state = {
-        inputValue: ""
+        firstName: "",
+        lastName: "",
     }
     onHandleChange = (event) => {
         console.log(event.target.value)
         this.setState({
-            inputValue: event.target.value
+            [event.target.name]:event.target.value
         })
     }
     onSubmit = (event) => {
@@ -20,9 +21,11 @@ class Forms extends Component {
             <div>
                 <form action="">
                     <h2>Display Input</h2>
-                    <input type="text" value={this.state.inputValue} onChange={this.onHandleChange} />
+                    <input name="firstName" type="text" value={this.state.firstName} onChange={this.onHandleChange} />
+                    <input name="lastName" type="text" value={this.state.lastName} onChange={this.onHandleChange} />
                     <button type="button" onClick={this.onSubmit}>Submit</button></form>
-                <h4>{this.state.inputValue}</h4>
+                <h4>{this.state.firstName}</h4>
+                <h4>{this.state.lastName}</h4>
             </div>
         )
     }
